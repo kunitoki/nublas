674c31d8f6e8bcabc8d9b35bacd2b6c0ae8c9b05
kunitoki <kunitoki@gmail.com>
2014-09-11 00:18:33 +0200

    - added settings files for testing

---
+++ nublas/tests/settings.py
@@ -0,0 +1,60 @@
+# Django settings for testproject project.
+
+import os
+DIRNAME = os.path.dirname(__file__)
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+ADMINS = ()
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DIRNAME, 'db.sqlite3'),
+        'TEST_NAME': os.path.join(DIRNAME, 'test_db.sqlite3'),
+    }
+}
+
+TIME_ZONE = 'Europe/Rome'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+SECRET_KEY = 'vaO4Y<g#YRfG8;Md9noiLp>.w(w~q_c=|1`?9<x>0KxA%UB!63'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'nublas.tests.urls'
+TEMPLATE_DIRS = ()
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+
+    'nublas',
+    'nublas.tests',
+)
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+STATIC_URL = '/static/'
---
+++ nublas/tests/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples for custom menu
+    url(r'^admin/', include(admin.site.urls)),
+)
